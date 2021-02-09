51d129221e26da1595051d6e457f3b68b0351140
hernandesjunio <hernandesjunio@gmail.com>
2021-02-09 15:59:14 -0300

    📦 refactor: material modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,21 +6,39 @@ import { AppComponent } from './app.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MatTableModule } from '@angular/material/table';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import {MatDialogModule} from '@angular/material/dialog';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatCardModule } from '@angular/material/card';
+import { MatSelectModule } from '@angular/material/select';
+import { MatListModule } from '@angular/material/list';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { FormComponent } from './form/form.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { DisplayComponent } from './display/display.component';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, FormComponent, DisplayComponent],
   imports: [
+    MatInputModule,
+    BrowserAnimationsModule,
     BrowserModule,
+    MatButtonModule,
     AppRoutingModule,
     ReactiveFormsModule,
     MatTableModule,
     MatFormFieldModule,
-    MatDialogModule
+    MatDialogModule,
+    MatCardModule,
+    MatSelectModule,
+    FlexLayoutModule,
+    HttpClientModule,
+    MatListModule,
+    MatIconModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
